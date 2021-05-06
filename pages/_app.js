706f220b220b40fab1f17101a088fccb2b30c102
Celia Saumell <56240891+celiasaumell@users.noreply.github.com>
2021-05-05 22:22:04 -0500

    Revert "Merge pull request #3 from celiasaumell/header"
    This reverts commit 2153c64bc10b7fe8de90f668ea5489b9f8059bee, reversing
    changes made to 6dee61d6cdfd82ad5fad6d7fe17630a50c193895.

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import '../styles/normalize.css'
+import '../styles/global.sass'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export default function App({Component, pageProps}) {
+    return <Component {...pageProps} />
+}
