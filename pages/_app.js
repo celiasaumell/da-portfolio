59a6c7fdfcc34e2fc983958cef04fa6f936d328b
Celia Saumell <56240891+celiasaumell@users.noreply.github.com>
2021-05-05 21:32:47 -0500

    Fix casing discrepancies.

--- pages/_app.js
+++
@@ -1,6 +0,0 @@
-import '../styles/normalize.css'
-import '../styles/global.sass'
-
-export default function App({Component, pageProps}) {
-    return <Component {...pageProps} />
-}
--- pages/about.js
+++
@@ -1,8 +0,0 @@
-import Head from "next/head";
-import Link from 'next/link'
-import Layout, { siteTitle } from "../components/layout";
-import Container from "react-bootstrap/Container"
-
-export default function About() {
-    return (<Layout><Container className="position-relative"><h1>Hi, I'm a data analyst passionate about... </h1></Container></Layout>)
-}
