8139c20e0e985325ed22abfc930b40599b462880
Celia Saumell <56240891+celiasaumell@users.noreply.github.com>
2021-05-05 20:19:26 -0500

    removed unnecessary dependencies

--- components/navigation.js
+++ components/navigation.js
@@ -1,10 +1,10 @@
 import Nav from "react-bootstrap/Nav"
 import Navbar from "react-bootstrap/Navbar"
 
-export default function Navigation() {
+export default function Navigation(props) {
     return (
     <>
-    <Navbar bg="clear" variant="dark" expand="lg" fixed="top" >
+    <Navbar bg="clear" variant="dark" expand="lg" fixed="top">
         <Navbar.Brand href="/">Moises Figueroa</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
@@ -13,7 +13,7 @@ export default function Navigation() {
             <Nav.Link href="/about">About</Nav.Link>
             <Nav.Link href="/portfolio">Portfolio</Nav.Link>
             <Nav.Link href="#link">Contact</Nav.Link>
-            <Nav.Link href="/resume.pdf">Resume</Nav.Link>
+            <Nav.Link href="/resume.pdf" target="_blank">Resume</Nav.Link>
           </Nav>
         </Navbar.Collapse>
     </Navbar>
